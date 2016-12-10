c2f18941d17f26fee8d56f53835dc8ba117f25c7
David Lecoconnier <david.lecoconnier@free.fr>
2016-12-10 19:28:34 +0100

    Project independency

---
+++ include/tools/database/DataBaseMetaData.h
@@ -0,0 +1,10 @@
+#ifndef DATABASEMETADATA_H_INCLUDED
+#define DATABASEMETADATA_H_INCLUDED
+
+#define DataBaseVersion "version"
+
+#define TABLE_META_DATA "meta_data"
+#define COLUMN_DATA_KEY "key"
+#define COLUMN_DATA_VALUE "value"
+
+#endif // DATABASEMETADATA_H_INCLUDED
